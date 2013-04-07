6f34aabd32c677a526049a5babb13f42421f8607
Eric Tobin <eatobin@gmail.com>
2013-04-06 20:50:16 -0500

    progress

--- src/main/roster.rb
+++ src/main/roster.rb
@@ -1,13 +1,28 @@
 require_relative('../main/player')
 
 class Roster
-  attr_reader(:roster_list)
+  attr_reader (:roster_list)
 
   def initialize
     @roster_list = Hash.new
-    @roster_list = {:SarArt => Player.new('Sarah Artzi', 'JerCoh'),
-                    :EriTob => Player.new('Eric TobinZZ', 'SarArt'),
-                    :JerCoh => Player.new('Jerri Cohen', 'EriTob')
+    @roster_list = {:AdaBur => Player.new('Adam Burish', 'DunKei'),
+                    :AndLad => Player.new('Andrew Ladd', 'JoeQue'),
+                    :AntNie => Player.new('Antti Niemi', 'JonToe'),
+                    :BreSea => Player.new('Brent Seabrook', 'KriVer'),
+                    :BryBic => Player.new('Bryan Bickell', 'MarHos'),
+                    :BriCam => Player.new('Brian Campbell', 'NikHja'),
+                    :CriHue => Player.new('Cristobal Huet', 'PatKan'),
+                    :DavBol => Player.new('Dave Bolland', 'PatSha'),
+                    :DunKei => Player.new('Duncan Keith', 'TomKop'),
+                    :JoeQue => Player.new('Joel Quenneville', 'TroBro'),
+                    :JonToe => Player.new('Jonathan Toews', 'AdaBur'),
+                    :KriVer => Player.new('Kris Versteeg', 'AndLad'),
+                    :MarHos => Player.new('Marian Hossa', 'AntNie'),
+                    :NikHja => Player.new('Niklas Hjalmarsson', 'BreSea'),
+                    :PatKan => Player.new('Patrick Kane', 'BryBic'),
+                    :PatSha => Player.new('Patrick Sharp', 'BriCam'),
+                    :TomKop => Player.new('Tomas Kopecky', 'CriHue'),
+                    :TroBro => Player.new('Troy Brouwer', 'DavBol'),
     }
   end
 end
