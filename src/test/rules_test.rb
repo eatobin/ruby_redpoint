922afe307cf612b488b395f6fd64422cebb3eafc
Eric Tobin <eatobin@gmail.com>
2013-04-14 17:44:22 -0500

    rule3 complete and tests

--- src/test/rules_test.rb
+++ src/test/rules_test.rb
@@ -10,5 +10,21 @@ class RulesTest < Test::Unit::TestCase
     blackhawks2010.set_givee_code(:PatSha, :AdaBur, 1)
     assert_true(Rules.givee_not_repeat(:PatSha, :AdaBur, blackhawks2010, 1))
     assert_false(Rules.givee_not_repeat(:PatSha, :BriCam, blackhawks2010, 1))
+    blackhawks2010.add_new_year
+    blackhawks2010.set_givee_code(:PatSha, :AndLad, 2)
+    blackhawks2010.add_new_year
+    blackhawks2010.set_givee_code(:PatSha, :AntNie, 3)
+    blackhawks2010.add_new_year
+    blackhawks2010.set_givee_code(:PatSha, :BreSea, 4)
+    blackhawks2010.add_new_year
+    blackhawks2010.set_givee_code(:PatSha, :BryBic, 5)
+    blackhawks2010.add_new_year
+    blackhawks2010.set_givee_code(:PatSha, :BriCam, 6)
+    assert_true(Rules.givee_not_repeat(:PatSha, :AdaBur, blackhawks2010, 6))
+    assert_true(Rules.givee_not_repeat(:PatSha, :BriCam, blackhawks2010, 6))
+    assert_false(Rules.givee_not_repeat(:PatSha, :AdaBur, blackhawks2010, 5))
+    assert_true(Rules.givee_not_repeat(:PatSha, :BriCam, blackhawks2010, 5))
+    assert_false(Rules.givee_not_repeat(:PatSha, :BriCam, blackhawks2010, 4))
+    assert_false(Rules.givee_not_repeat(:PatSha, :BryBic, blackhawks2010, 6))
   end
 end
