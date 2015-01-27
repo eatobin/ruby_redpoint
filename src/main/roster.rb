8f455d8b968228ac81058081c0d1b594a3e6915b
Eric Tobin <eatobin@gmail.com>
2015-01-27 14:27:32 -0600

    constructor works

--- src/main/roster.rb
+++ src/main/roster.rb
@@ -1,114 +1,94 @@
 class Roster
   attr_reader(:team_name, :first_year, :roster_list)
 
-  def initialize
-    # define an array to hold the Person records
-    people = Array.new
-
-# open the csv file
-    f = File.open("people.csv", "r")
-
-# loop through each record in the csv file, adding
-# each record to our array.
-    f.each_line { |line|
-
-      # each line has fields separated by commas, so split those fields
-      fields = line.split(',')
-
-      # create a new Person
-      p = Person.new
-
-      # do a little work here to get rid of double-quotes and blanks
-      p.last_name = fields[0].tr_s('"', '').strip
-      p.first_name = fields[1].tr_s('"', '').strip
-      p.city = fields[2].tr_s('"', '').strip
-      people.push(p)
-    }
-
-    # @roster_list = {
-    #     :TroBro => Player.new('Troy Brouwer', :DavBol),
-    #     :AdaBur => Player.new('Adam Burish', :DunKei),
-    #     :AndLad => Player.new('Andrew Ladd', :JoeQue),
-    #     :AntNie => Player.new('Antti Niemi', :JonToe),
-    #     :BreSea => Player.new('Brent Seabrook', :KriVer),
-    #     :BryBic => Player.new('Bryan Bickell', :MarHos),
-    #     :BriCam => Player.new('Brian Campbell', :NikHja),
-    #     :CriHue => Player.new('Cristobal Huet', :PatKan),
-    #     :DavBol => Player.new('Dave Bolland', :PatSha),
-    #     :DunKei => Player.new('Duncan Keith', :TomKop),
-    #     :JoeQue => Player.new('Joel Quenneville', :TroBro),
-    #     :JonToe => Player.new('Jonathan Toews', :AdaBur),
-    #     :KriVer => Player.new('Kris Versteeg', :AndLad),
-    #     :MarHos => Player.new('Marian Hossa', :AntNie),
-    #     :NikHja => Player.new('Niklas Hjalmarsson', :BreSea),
-    #     :PatKan => Player.new('Patrick Kane', :BryBic),
-    #     :PatSha => Player.new('Patrick Sharp', :BriCam),
-    #     :TomKop => Player.new('Tomas Kopecky', :CriHue)
-    # }
+  def initialize(file_name)
+    @roster_list = Hash.new
+    begin
+      f = File.open(file_name, 'r')
+      f.each_line { |line|
+        fields = line.split(',')
+        if fields.length == 2
+          @team_name = fields[0].strip
+          @first_year = fields[1].strip
+        else
+          @roster_list[fields[0].strip.to_sym] = Player.new(fields[1].strip, fields[2].strip.to_sym, fields[3].strip.to_sym)
+        end
+      }
+      f.close
+        # p(@team_name)
+        # p(@first_year)
+        # p(@roster_list)
+    rescue
+      puts 'File Read error here!'
+    end
   end
 
-  private
   class Player
-    attr_reader(:player_name, :past_givee_codes)
+    attr_reader(:player_name, :gift_history)
 
-    def initialize(player_name, givee_code_year_zero)
+    def initialize(player_name, givee_code_year_zero, giver_code_year_zero)
       @player_name = player_name
-      @past_givee_codes = Array.new
-      @past_givee_codes << givee_code_year_zero
+      @gift_history = Array.new
+      roles = Array.new
+      givee = 0
+      giver = 1
+      roles[givee] = givee_code_year_zero
+      roles[giver] = giver_code_year_zero
+      @gift_history << roles
     end
 
-    def add_givee_code(givee_code)
-      @past_givee_codes << givee_code
-    end
-
-    def return_givee_code(gift_year)
-      @past_givee_codes.fetch(gift_year)
-    end
-
-    def set_givee_code(givee_code, year)
-      @past_givee_codes[year] = givee_code
-    end
+    #   def add_givee_code(givee_code)
+    #     @past_givee_codes << givee_code
+    #   end
+    #
+    #   def return_givee_code(gift_year)
+    #     @past_givee_codes.fetch(gift_year)
+    #   end
+    #
+    #   def set_givee_code(givee_code, year)
+    #     @past_givee_codes[year] = givee_code
+    #   end
   end
-
-  public
-  def return_player(player_code)
-    @roster_list.fetch(player_code)
-  end
-
-  def set_givee_code(player_code, givee_code, year)
-    self.return_player(player_code).set_givee_code(givee_code, year)
-  end
-
-  def return_givee_code(player_code, year)
-    self.return_player(player_code).return_givee_code(year)
-  end
-
-  def add_new_year
-    @roster_list.each_value do |player|
-      player.add_givee_code(:none)
-    end
-  end
-
-  def giving_roster_report_string(year)
-    rr = String.new
-    Hash[@roster_list.sort_by { |player_code| player_code }].each_value do |player|
-      player_name = player.player_name
-      givee_code = player.return_givee_code(year)
-      if givee_code.eql?(:none)
-        givee_name = '...nobody!! (last giver/givee pairing and a test failed - a puzzle logic error)'
-      else
-        givee_name = self.return_player(givee_code).player_name
-      end
-      rr << player_name + " is buying for " + givee_name + "\n"
-    end
-    rr
-  end
-
-  def print_and_ask(year)
-    puts "Year #{year} Gifts:"
-    puts self.giving_roster_report_string(year)
-    puts
-    print 'Continue? (1 = yes, 0 = no): '
-    gets.chomp.to_i
-  end
+  #
+  # public
+  # def return_player(player_code)
+  #   @roster_list.fetch(player_code)
+  # end
+  #
+  # def set_givee_code(player_code, givee_code, year)
+  #   self.return_player(player_code).set_givee_code(givee_code, year)
+  # end
+  #
+  # def return_givee_code(player_code, year)
+  #   self.return_player(player_code).return_givee_code(year)
+  # end
+  #
+  # def add_new_year
+  #   @roster_list.each_value do |player|
+  #     player.add_givee_code(:none)
+  #   end
+  # end
+  #
+  # def giving_roster_report_string(year)
+  #   rr = String.new
+  #   Hash[@roster_list.sort_by { |player_code| player_code }].each_value do |player|
+  #     player_name = player.player_name
+  #     givee_code = player.return_givee_code(year)
+  #     if givee_code.eql?(:none)
+  #       givee_name = '...nobody!! (last giver/givee pairing and a test failed - a puzzle logic error)'
+  #     else
+  #       givee_name = self.return_player(givee_code).player_name
+  #     end
+  #     rr << player_name + " is buying for " + givee_name + "\n"
+  #   end
+  #   rr
+  # end
+  #
+  # def print_and_ask(year)
+  #   puts "Year #{year} Gifts:"
+  #   puts self.giving_roster_report_string(year)
+  #   puts
+  #   print 'Continue? (1 = yes, 0 = no): '
+  #   gets.chomp.to_i
+  # end
 end
--- src/test/roster_test.rb
+++ src/test/roster_test.rb
@@ -4,28 +4,28 @@ require 'redgreen'
 
 class RosterTest < Test::Unit::TestCase
   def test_return_givee
-    blackhawks2010 = Roster.new
-    assert_equal(:DunKei, blackhawks2010.return_givee_code(:AdaBur, 0))
-    assert_equal(:BriCam, blackhawks2010.return_givee_code(:PatSha, 0))
+    blackhawks2010 = Roster.new('../../blackhawks2010.txt')
+    # assert_equal(:DunKei, blackhawks2010.return_givee_code(:AdaBur, 0))
+    # assert_equal(:BriCam, blackhawks2010.return_givee_code(:PatSha, 0))
   end
 
-  def test_set_givee
-    blackhawks2010 = Roster.new
-    assert_equal(:DunKei, blackhawks2010.return_givee_code(:AdaBur, 0))
-    assert_equal(:MarHos, blackhawks2010.set_givee_code(:AdaBur, :MarHos, 0))
-    assert_equal(:MarHos, blackhawks2010.return_givee_code(:AdaBur, 0))
-  end
-
-  def test_add_new_year
-    blackhawks2010 = Roster.new
-    blackhawks2010.add_new_year
-    assert_equal(:DunKei, blackhawks2010.return_givee_code(:AdaBur, 0))
-    assert_equal(:none, blackhawks2010.return_givee_code(:AdaBur, 1))
-    assert_equal(:none, blackhawks2010.return_givee_code(:PatSha, 1))
-  end
-
-  def test_print_giving_roster
-    blackhawks2010 = Roster.new
-    puts blackhawks2010.giving_roster_report_string(0)
-  end
+  # def test_set_givee
+  #   blackhawks2010 = Roster.new
+  #   assert_equal(:DunKei, blackhawks2010.return_givee_code(:AdaBur, 0))
+  #   assert_equal(:MarHos, blackhawks2010.set_givee_code(:AdaBur, :MarHos, 0))
+  #   assert_equal(:MarHos, blackhawks2010.return_givee_code(:AdaBur, 0))
+  # end
+  #
+  # def test_add_new_year
+  #   blackhawks2010 = Roster.new
+  #   blackhawks2010.add_new_year
+  #   assert_equal(:DunKei, blackhawks2010.return_givee_code(:AdaBur, 0))
+  #   assert_equal(:none, blackhawks2010.return_givee_code(:AdaBur, 1))
+  #   assert_equal(:none, blackhawks2010.return_givee_code(:PatSha, 1))
+  # end
+  #
+  # def test_print_giving_roster
+  #   blackhawks2010 = Roster.new
+  #   puts blackhawks2010.giving_roster_report_string(0)
+  # end
 end
